f397ec22f399b7d0ac58633908fed1bf1549fb3e
Marian Guceanu <george.guceanu@stud.ubbcluj.ro>
2022-07-11 17:49:20 +0300

    lesgo

--- Board/board.py
+++ Board/board.py
@@ -6,8 +6,8 @@ class Board:
         The board for the obstruction game, size of the board is given.
         Size must be greater or equal to 6, because otherwise none of the players can move.
     """
-    def __init__(self, size):
-        if size <= 6:
+    def __init__(self, size:int):
+        if size < 6:
             raise BoardException("Size of board is not enough")
         self.__size = size
         self.__bd = list()
@@ -20,14 +20,91 @@ class Board:
         """
             Setter, throws error in case of wrong input.
         """
-        if position[0] < 0 or position[0] >6:
+        x = position[0] - 1
+        y = position[1] - 1
+        if position[0] < 1 or position[0] >6:
             raise BoardException("Invalid positions")
-        if position[1] < 0 or position[1] > 6:
+        if position[1] < 1 or position[1] > 6:
             raise BoardException("Invalid positions")
-        self.__bd[position[0]-1][position[1]-1]=player
-    
+        if self.__bd[x][y] != 'x ':
+            self.__bd[x][y] = str(player) + " "
+            
+            # Checking corner cases
+
+            if x == 0 and y == 0: # Top left corner
+                self.__bd[x+1][y] = "x "
+                self.__bd[x+1][y+1] = "x "
+                self.__bd[x][y+1] = "x "
+                return
+            
+            if x == 0 and y == self.__size - 1: # Top right corner
+                self.__bd[x][y-1] = "x "
+                self.__bd[x+1][y] = "x "
+                self.__bd[x+1][y-1] = "x "
+                return
+
+            if x == self.__size - 1 and y == 0: # Bottom left corner
+                self.__bd[x-1][y] = "x "
+                self.__bd[x][y+1] = "x "
+                self.__bd[x-1][y+1] = "x "
+                return
+
+            if x == self.__size - 1 and y == self.__size - 1: # Bottom right corner
+                self.__bd[x-1][y-1] = "x "
+                self.__bd[x-1][y] = "x "
+                self.__bd[x][y-1] = "x "
+                return
+
+            # Checking rail cases
+            if x == 0 and y != 0 and y != self.__size - 1: # Top rail
+                self.__bd[x][y-1] = "x "
+                self.__bd[x+1][y-1] = "x "
+                self.__bd[x+1][y] = "x "
+                self.__bd[x+1][y+1] = "x "
+                self.__bd[x][y+1] = "x "
+                return
+
+            if x == self.__size - 1 and y != 0 and y != self.__size - 1: # Bottom rail
+                self.__bd[x][y-1] = "x "
+                self.__bd[x-1][y] = "x "
+                self.__bd[x-1][y+1] = "x "
+                self.__bd[x-1][y-1] = "x "
+                self.__bd[x][y+1] = "x "
+                return
+
+            if y == 0 and x != 0 and x != self.__size - 1: # Left rail
+                self.__bd[x-1][y] = "x "
+                self.__bd[x][y+1] = "x "
+                self.__bd[x-1][y+1] = "x "
+                self.__bd[x+1][y+1] = "x "
+                self.__bd[x+1][y] == "x "
+                return
+
+            if y == self.__size - 1 and x != 0 and x!= self.__size - 1: # Right rail
+                self.__bd[x-1][y] = "x "
+                self.__bd[x+1][y] = "x "
+                self.__bd[x-1][y-1] = "x "
+                self.__bd[x+1][y-1] = "x "
+                self.__bd[x][y-1] = "x "
+                return
+
+            # The rest of the pieces
+            self.__bd[x-1][y-1] = "x "   # Top left box
+            self.__bd[x-1][y] = "x "     # Top box
+            self.__bd[x-1][y+1] = "x "   # Top right box
+            self.__bd[x][y-1] = "x "     # Left box
+            self.__bd[x][y+1] = "x "    # Right box
+            self.__bd[x+1][y-1] = "x "   # Bottom left box
+            self.__bd[x+1][y] = "x "     # Bottom box
+            self.__bd[x+1][y+1] = "x "   # Bottom right box
+            return
+
+        raise BoardException("Wrong position fam")
 
     def __str__(self):
+        """
+            Overriding the __str__ function so that we can use str(Board) to print it.
+        """
         final_str = ""
         for i in range(self.__size):
             for j in range(self.__size):
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from Board.board import Board
 
 def __main__():
-    b = Board(5)
+    b = Board(6)
     b.setPosition((2, 3), 1)
+    b.setPosition((4,2), 2)
     print(b.__str__())
+    b.setPosition((2,4), 1)
 
 
 if __name__=="__main__":
